568a13241cb77fe18f61fe8d053a43f653d7f9df
Fairbrook <kevinvr@hotmail.es>
2022-02-28 16:00:55 -0600

    random

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -10,8 +10,6 @@ extern crate native_windows_gui as nwg;
 
 pub fn start() -> Result<(), Box<dyn std::error::Error>> {
     nwg::init().expect("Fallo la inicialización");
-    let _app = windows::Dialog::build_ui(Default::default()).expect("Fallo al construir el ui");
-    nwg::dispatch_thread_events();
     let _app = windows::Main::build_ui(Default::default()).expect("Fallo al construir el ui");
     nwg::dispatch_thread_events();
     Ok(())
--- src/ui/windows/mod.rs
+++ src/ui/windows/mod.rs
@@ -1,28 +1,57 @@
 use nwd::NwgUi;
+use nwg::stretch::{
+    geometry::Size,
+    style::{Dimension as D, FlexDirection},
+};
 extern crate native_windows_gui as nwg;
+use std::{cell::RefCell, thread};
 
 #[derive(Default, NwgUi)]
-pub struct Main{
-    #[nwg_control(size: (300, 115), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
-    #[nwg_events( OnWindowClose: [Main::close] )]
+pub struct Main {
+    dialog_data: RefCell<Option<thread::JoinHandle<String>>>,
+
+    #[nwg_control(size: (300, 115), position: (300, 300), title: "Simulador de procesos", flags: "WINDOW|VISIBLE")]
+    #[nwg_events( OnWindowClose: [Main::close], OnInit:[Main::open_dialog] )]
     window: nwg::Window,
 }
 
-impl Main{
-    fn close(&self){
+impl Main {
+    fn close(&self) {
         nwg::stop_thread_dispatch();
     }
+    fn open_dialog(&self) {}
 }
 
 #[derive(Default, NwgUi)]
-pub struct Dialog{
-    #[nwg_control(size: (300, 115), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
+pub struct Dialog {
+    data: RefCell<Option<String>>,
+    #[nwg_control(size: (300, 115), position: (300, 300), title: "Numero de procesos", flags: "WINDOW|VISIBLE")]
     #[nwg_events( OnWindowClose: [Dialog::close] )]
     window: nwg::Window,
+
+    #[nwg_layout(parent: window, flex_direction: FlexDirection::Row)]
+    layout: nwg::FlexboxLayout,
+
+    #[nwg_control]
+    #[number_select]
+    #[nwg_layout_item(layout: layout,
+        // margin: MARGIN,
+        max_size: Size { width: D::Points(200.0), height: D::Undefined },
+        size: Size { width: D::Percent(0.5), height: D::Auto }
+    )]
+    input: nwg::NumberSelect,
 }
 
-impl Dialog{
-    fn close(&self){
+impl Dialog {
+    fn close(&self) {
         nwg::stop_thread_dispatch();
+    }
+    fn ask_number(sender: nwg::NoticeSender) -> thread::JoinHandle<String> {
+        thread::spawn(move || {
+            let dialog = Dialog::build_ui(Default::default()).expect("Error al crear el díalogo");
+            ngw::dispatch_thread_events(); 
+            sender.notice();
+            dialog.
+        })
     }
 }
